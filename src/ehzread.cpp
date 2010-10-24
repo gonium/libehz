f2d38acc2cb52c88f2a2e439c664e08c755a1c45
Mathias Dalheimer <md@gonium.net>
2010-10-24 19:15:27 +0200

    added GPLv3 copyright notice

--- src/ehzread.cpp
+++ src/ehzread.cpp
@@ -1,3 +1,23 @@
+/**
+ * This file is part of libehz.
+ *
+ * (c) Mathias Dalheimer <md@gonium.net>, 2010
+ *
+ * libehz is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libehz is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libehz. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <cstdlib>
 #include <iostream>
 #include <cstring>
@@ -27,9 +47,9 @@ int main(int argc,char** argv) {
   cfsetispeed(&tio,B9600);            // 9600 baud
 
   tcsetattr(tty_fd,TCSANOW,&tio);
-  while (true)
-  {
-    if (read(tty_fd,&c,1)>0) // if new data is available on the serial port, print it out
+  while (true) {
+    if (read(tty_fd,&c,1)>0) 
+      // if new data is available on the serial port, print it out
       std::cout << &c;
   }
 
