fbd4340c8a94c9923a85dfb0515173ef703a615e
Mathias Dalheimer <md@gonium.net>
2010-10-24 18:49:39 +0200

    removed stdin

--- src/ehzread.cpp
+++ src/ehzread.cpp
@@ -1,44 +1,42 @@
 #include <cstdlib>
+#include <iostream>
 #include <cstring>
-#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <termios.h>
 
 int main(int argc,char** argv) {
   struct termios tio;
-  struct termios stdio;
+ // struct termios stdio;
   int tty_fd;
   fd_set rdset;
 
   unsigned char c='D';
 
-  printf("Please start with %s /dev/ttyS1 (for example)\n",argv[0]);
-  memset(&stdio,0,sizeof(stdio));
-  stdio.c_iflag=0;
-  stdio.c_oflag=0;
-  stdio.c_cflag=0;
-  stdio.c_lflag=0;
-  stdio.c_cc[VMIN]=1;
-  stdio.c_cc[VTIME]=0;
-  tcsetattr(STDOUT_FILENO,TCSANOW,&stdio);
-  tcsetattr(STDOUT_FILENO,TCSAFLUSH,&stdio);
+  std::cout << "Please start with " << argv[0] << 
+    " /dev/ttyS1 (for example)" << std::endl;
+ // memset(&stdio,0,sizeof(stdio));
+ // stdio.c_iflag=0;
+ // stdio.c_oflag=0;
+ // stdio.c_cflag=0;
+ // stdio.c_lflag=0;
+ // stdio.c_cc[VMIN]=1;
+ // stdio.c_cc[VTIME]=0;
+ // tcsetattr(STDOUT_FILENO,TCSANOW,&stdio);
+ // tcsetattr(STDOUT_FILENO,TCSAFLUSH,&stdio);
   fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);       // make the reads non-blocking
-
-
-
 
   memset(&tio,0,sizeof(tio));
   tio.c_iflag=0;
   tio.c_oflag=0;
-  tio.c_cflag=CS7|CREAD|CLOCAL;           // 8n1, see termios.h for more information
+  tio.c_cflag=CS7|CREAD|CLOCAL;           // 7n1, see termios.h for more information
   tio.c_lflag=0;
   tio.c_cc[VMIN]=1;
   tio.c_cc[VTIME]=5;
 
   tty_fd=open(argv[1], O_RDWR | O_NONBLOCK);      
-  cfsetospeed(&tio,B9600);            // 115200 baud
-  cfsetispeed(&tio,B9600);            // 115200 baud
+  cfsetospeed(&tio,B9600);            // 9600 baud
+  cfsetispeed(&tio,B9600);            // 9600 baud
 
   tcsetattr(tty_fd,TCSANOW,&tio);
   while (c!='q')
