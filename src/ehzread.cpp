1da63e6094ea70db2e8d848dccfcd6f3e10cea53
Matthias Klein <kleinmat@thinkpad.(none)>
2010-10-25 15:04:56 +0200

    Non-blocking IO activated

--- src/ehzread.cpp
+++ src/ehzread.cpp
@@ -37,14 +37,14 @@ int main(int argc,char** argv) {
   memset(&tio,0,sizeof(tio));
   tio.c_iflag=0;
   tio.c_oflag=0;
-  tio.c_cflag=CS7|CREAD|CLOCAL;           // 7n1, see termios.h for more information
+  tio.c_cflag=CS7|CREAD|CLOCAL;			// 7n1, see termios.h for more information
   tio.c_lflag=0;
   tio.c_cc[VMIN]=1;
   tio.c_cc[VTIME]=5;
 
-  tty_fd=open(argv[1], O_RDWR | O_NONBLOCK);      
-  cfsetospeed(&tio,B9600);            // 9600 baud
-  cfsetispeed(&tio,B9600);            // 9600 baud
+  tty_fd=open(argv[1], O_RDWR);			//  | O_NONBLOCK);
+  cfsetospeed(&tio,B9600);			// 9600 baud
+  cfsetispeed(&tio,B9600);			// 9600 baud
 
   tcsetattr(tty_fd,TCSANOW,&tio);
   while (true) {
