19240c97e4ff19fbdf5082bc2f15369dc22cd17c
kleinmat <matthias.klein@itwm.fraunhofer.de>
2010-11-08 11:04:44 +0100

    added Copyright headers...

--- src/EhzDatagram.cpp
+++ src/EhzDatagram.cpp
@@ -1,9 +1,23 @@
-/*
- * EhzDatagramm.cpp
- *
- *  Created on: 28.10.2010
- *      Author: kleinmat
- */
+/**
+* This file is part of libehz.
+*
+* (c) Mathias Dalheimer <md@gonium.net>, 2010
+* (c) Matthias Klein <name.surename @ itwm.fraunhofer.de>, 2010
+*
+* libehz is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* libehz is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with libehz. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 
 #include "EhzDatagram.h"
 #include <sstream>
--- src/ehzreader.cpp
+++ src/ehzreader.cpp
@@ -1,10 +1,23 @@
-//============================================================================
-// Name        : ehzreader.cpp
-// Author      : kleinmat
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+/**
+* This file is part of libehz.
+*
+* (c) Mathias Dalheimer <md@gonium.net>, 2010
+* (c) Matthias Klein <name.surename @ itwm.fraunhofer.de>, 2010
+*
+* libehz is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* libehz is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with libehz. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 
 #include <iostream>
 #include "EhzRead.h"
